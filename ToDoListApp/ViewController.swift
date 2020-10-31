0838d0fb2a443eaafad183bb5dc651ffdde61e36
Tomasz Plauszkiewicz <tpal.developer@gmail.com>
2020-10-31 13:29:06 +0100

    Initial Commit

---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Tomasz Paluszkiewicz on 31/10/2020.
+//  Copyright © 2020 Tomasz Paluszkiewicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
